5c96ee8ec19f8aa85d1ba35e1cd69e6366e45ab1
Denysyk <den.brtvnk@gmail.com>
2025-07-20 12:42:16 +0300

    Initial Commit

---
+++ Test_BrativnykTests/Test_BrativnykTests.swift
@@ -0,0 +1,17 @@
+//
+//  Test_BrativnykTests.swift
+//  Test_BrativnykTests
+//
+//  Created by Denys Brativnyk on 20.07.2025.
+//
+
+import Testing
+@testable import Test_Brativnyk
+
+struct Test_BrativnykTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Test_BrativnykUITests/Test_BrativnykUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Test_BrativnykUITestsLaunchTests.swift
+//  Test_BrativnykUITests
+//
+//  Created by Denys Brativnyk on 20.07.2025.
+//
+
+import XCTest
+
+final class Test_BrativnykUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
